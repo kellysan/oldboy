71cf20429dae1c558ef4779188f427ea9fe1c0eb
sanyapeng <sanyapeng@playcra.com>
2019-05-22 10:29:59 +0800

    up

---
+++ homework/day07/teacher.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    File Name：       teacher
+#    Description :
+#    Author :          SanYapeng
+#    date：            2019-05-22
+#    Change Activity:  2019-05-22:
+
+from base import InfoMode, Base, Database
+
+info = InfoMode()
+b= Base()
+db = Database()
+
+
+def examine_all_course():
+    course_info = db.db_select("course_info.json")
+    print("全部课程".center(20,"#"))
+    for c in course_info:
+        print("课程名称:{}\n"
+              "\t课程价格:{}\n" \
+              "\t课程时间{}".format(c, course_info[c].get("price"), course_info[c].get("hour")))
+    print("全部课程".center(20, "#"))
+    return True
+
+
+def examine_allot_class():
+    """
+    查找出老师所分配的班级
+    :return:
+    """
+    user_name = b.check_login_account("name")
+    user_info = db.db_select("user_info.json")
+    print("您所分配的班级:{}".format("|".join(user_info[user_name].get("class"))))
+    return True
+
+def examine_all_class_student():
+    """
+
+    :return: 显示所有班级的学生信息
+    """
+    user_name = b.check_login_account("name")
+    user_info = db.db_select("user_info.json")
+    class_info = db.db_select("class_info.json")
+    for c in user_info[user_name].get("class"):
+        pass
+
+
+
+if __name__ == '__main__':
+    examine_allot_class()
