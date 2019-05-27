1c4eac204e20ffca551b4711bec15da7e841661e
sanyapeng <sanyapeng@playcrab.com>
2019-05-28 07:03:07 +0800

    add homework

---
+++ homework/day08/__init__.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    File Name：       __init__.py
+#    Description :
+#    Author :          SanYapeng
+#    date：            2019-05-27
+#    Change Activity:  2019-05-27:
